817259823a8327dbff4c8c8e915eceae7973f4b3
LilianaVallejosLalangui <112661988+Lilian1990@users.noreply.github.com>
2023-01-18 16:38:12 -0500

    Add files via upload

---
+++ scripts/carrusel.js
@@ -0,0 +1,85 @@
+window.onload = function () {
+    // Variables
+    const IMAGENES = [
+    //Ingresamos los nobres de cada una de las imagenes que se encuentran en la carpeta img
+        'images/diseño.png',
+        'img/hierba.jpg',
+        'img/hoja.jpg',
+        'img/primavera.jpg',
+        'img/bella.jpg',
+        'img/baños.jpg'
+    ];
+    //Para poner el intervalo de tiempo entre las imagenes
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 2000;
+    let posicionActual = 0;
+    //Llamamos a cada uno de los botones que estan en carrusel.html
+    let $botonRetroceder = document.querySelector('#retroceder');
+    let $botonAvanzar = document.querySelector('#avanzar');
+    let $imagen = document.querySelector('#imagen');
+    let $botonPlay = document.querySelector('#play');
+    let $botonStop = document.querySelector('#stop');
+    let intervalo;
+
+    // Creamos la Funciones
+    /**
+     * Función que cambia la foto en la siguiente posicion
+     */
+    function pasarFoto() {
+        if(posicionActual >= IMAGENES.length - 1) {
+            posicionActual = 0;
+        } else {
+            posicionActual++;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Creamos la Función que cambia la foto en la anterior posición
+     */
+    function retrocederFoto() {
+        if(posicionActual <= 0) {
+            posicionActual = IMAGENES.length - 1;
+        } else {
+            posicionActual--;
+        }
+        renderizarImagen();
+    }
+
+    /**
+     * Función que actualiza la imagen de imagen dependiendo de posicionActual en la que se encuentre
+     */
+    function renderizarImagen () {
+        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+    }
+
+    /**
+     * Activa el autoplay de la imagen
+     */
+    function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+        // Desactivamos los botones de control
+        $botonAvanzar.setAttribute('disabled', true);
+        $botonRetroceder.setAttribute('disabled', true);
+        $botonPlay.setAttribute('disabled', true);
+        $botonStop.removeAttribute('disabled');
+    }
+
+    /**
+     * Para el stop de la imagen
+     */
+    function stopIntervalo() {
+        clearInterval(intervalo);
+        // Activamos los botones de control
+        $botonAvanzar.removeAttribute('disabled');
+        $botonRetroceder.removeAttribute('disabled');
+        $botonPlay.removeAttribute('disabled');
+        $botonStop.setAttribute('disabled', true);
+    }
+    // Eventos
+    $botonAvanzar.addEventListener('click', pasarFoto);
+    $botonRetroceder.addEventListener('click', retrocederFoto);
+    $botonPlay.addEventListener('click', playIntervalo);
+    $botonStop.addEventListener('click', stopIntervalo);
+    // Iniciar
+    renderizarImagen();
+} 
